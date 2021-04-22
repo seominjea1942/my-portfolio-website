d24a4b61beb882041780c045d5201149b656c7b2
seominjea1942 <seominjea1942@gmail.com>
2021-04-22 08:29:20 -0700

    basic layout done and start to make menu

--- src/components/HeroBox.js
+++ src/components/HeroBox.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import HeroBoxH1 from './HeroBoxH1'
 
 function HeroBox(props) {
+    let [marginTop, setMarginTop] = useState();
+
+    useEffect(()=>{
+        let hamburgerFirstBar = document.querySelector('.hamburger-react>div')
+        let hamburgerFirstBarHeight = parseInt(window.getComputedStyle(hamburgerFirstBar).getPropertyValue('height'))
+        let hamburgerFirstBarTop = parseInt(hamburgerFirstBar.getBoundingClientRect().top)
+        let adjustment = 2
+        let getMarginTop = hamburgerFirstBarTop - hamburgerFirstBarHeight -adjustment
+        setMarginTop(getMarginTop)
+    },[])
+
     return (
-        <div className="HeroBox">
+        <div className="HeroBox" style={{
+            marginTop: marginTop
+        }}>
             <h6>See Min's portfolio as:</h6>
-            <div>Video</div>
+            <HeroBoxH1></HeroBoxH1>
+            <div id='pfReel'>Video</div>
         </div>
     );
 }
---
+++ src/components/HeroBoxH1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeroBoxH1(props) {
+    return (
+        <div id='toggleAnimation'>
+            <h1>Digital Product Designer<span className="slash" >/</span>Software Engineer</h1>
+        </div>
+    );
+}
+
+export default HeroBoxH1;
